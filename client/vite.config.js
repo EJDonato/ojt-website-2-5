009be4708d3882198ca1cea66c98d9f1172f5a09
EJDonato <eltonjamesdonato05@gmail.com>
2025-07-27 13:04:17 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.VITE_BASE_PATH || '/ojt-website-2-5/tree/main/client',
   plugins: [
     react(),
     tailwindcss(),
